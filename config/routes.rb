af3cd6a2801411e38d65296b5c5c3ed488c7cadd
yclo2016 <neversee.paul@gmail.com>
2016-06-09 18:51:42 +0800

    完成第二週作業

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,27 @@
 Rails.application.routes.draw do
   root "products#index"
-  
+
   devise_for :users
   namespace :admin do
     resources :products
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
     resources :users do
       member do
         post :to_admin
         post :to_normal
       end
     end
+  end
+
+  namespace :account do
+    resources :orders
   end
 
   resources :orders do
@@ -22,10 +34,11 @@ Rails.application.routes.draw do
   resources :carts do
     collection do
       post :checkout
+      delete :clean
     end
   end
 
-
+  resources :items, controller: "cart_items"
 
   resources :products do
     member do
